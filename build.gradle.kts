07e371724a8ee0dd9e5f3b9fbff30de5b81396f4
queirozdaniel <danielqueirozak77@gmail.com>
2022-06-16 22:15:08 -0300

    chore: Added Dependence of Html DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,5 +20,6 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:1.6.4")
     implementation("io.ktor:ktor-server-netty:1.6.4")
+    implementation("io.ktor:ktor-server-html-builder:1.6.4")
     implementation("ch.qos.logback:logback-classic:1.2.5")
 }
